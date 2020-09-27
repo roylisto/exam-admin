bcee42e08bac95d470a5b8bf027609feeb76d377
Roy Pradana <roylistoputra@gmail.com>
2020-09-27 22:43:41 +0700

    change seeders

--- database/seeders/20190818182930-seed-admin.js
+++ database/seeders/20190818182930-seed-admin.js
@@ -6,17 +6,10 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     const salt = bcrypt.genSaltSync(10);
     return queryInterface.bulkInsert('admin', [{
-      name: 'Superadmin',
-      email: 'superadmin@educasia.id',
-      password: bcrypt.hashSync('Palembang#2021', salt),
-      role: 'superadmin',
-      created_at: new Date(),
-      updated_at: new Date(),
-    }, {
       name: 'Admin',
       email: 'admin@bakatku.id',
       password: bcrypt.hashSync('BinaHuman#2020', salt),
-      role: 'admin',
+      role: 'superadmin',
       created_at: new Date(),
       updated_at: new Date(),
     }], {});
