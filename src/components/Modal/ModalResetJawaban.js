2a3f0bab9187efd50e870cd53b15852f7e4fbfa9
Roy Pradana <roy@Roys-MacBook-Pro.local>
2020-11-05 10:06:08 +0700

    add dynamic checkbox

--- src/components/Modal/ModalResetJawaban.js
+++ src/components/Modal/ModalResetJawaban.js
@@ -4,6 +4,32 @@ import "./Modal.scss"
 import Button from "../Button"
 
 class ModalResetJawaban extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        ist: [
+          { label: 'Subtest 1', key: 'subtest_1_ist' },
+          { label: 'Subtest 2', key: 'subtest_2_ist' },
+          { label: 'Subtest 3', key: 'subtest_3_ist' },
+          { label: 'Subtest 4', key: 'subtest_4_ist' },
+          { label: 'Subtest 5', key: 'subtest_5_ist' },
+          { label: 'Subtest 6', key: 'subtest_6_ist' },
+          { label: 'Subtest 7', key: 'subtest_7_ist' },
+          { label: 'Subtest 8', key: 'subtest_8_ist' },
+          { label: 'Subtest 9', key: 'subtest_9_ist' }
+        ],
+        mii: [
+          { label: 'Bagian 1', key: 'bagian_1_verb_ling' },
+          { label: 'Bagian 2', key: 'bagian_2_log_math' },
+          { label: 'Bagian 3', key: 'bagian_3_spat' },
+          { label: 'Bagian 4', key: 'bagian_4_mus' },
+          { label: 'Bagian 5', key: 'bagian_5_bod_kin' },
+          { label: 'Bagian 6', key: 'bagian_6_inter' },
+          { label: 'Bagian 7', key: 'bagian_7_intra' },
+          { label: 'Bagian 8', key: 'bagian_8_nat' }
+        ],
+      }
+    }
 
     render(){
         return (
@@ -49,23 +75,45 @@ class ModalResetJawaban extends React.Component {
                                     readOnly={true}
                                 />
                             </div>
-                            <div className="form-group">
-                                <label>IST</label>
-                                <p>{this.props.errors.jenis_test}</p>
-                                <div
-                                  className={`form-control form-check form-check-inline ${this.props.errors.jenis_test ? "invalid" : ""}`}
-                                >
-                                  <input type="checkbox" className="form-check-input"
-                                    id="ist"
-                                    name="subtest_1_ist"
-                                    />
-                                  <label className="form-check-label">Subtest 1</label>
-                                  <input type="checkbox" className="form-check-input"
-                                    id="ist"
-                                    name="subtest_2_ist"
-                                    />
-                                  <label className="form-check-label">Subtest 2</label>
+                            <div className="row">
+                              <div className="col-lg-6">
+                                <div className="form-group">
+                                  <label>IST</label>
+                                  <p>{this.props.errors.jenis_test}</p>
+                                  <div
+                                    className={`mt-0 form-control form-check form-check-inline d-inline-block ${this.props.errors.jenis_test ? "invalid" : ""}`}
+                                  >
+                                    {this.state.ist.map((item, index) => {
+                                      return <div key={index}>
+                                              <input type="checkbox" className="form-check-input"
+                                                id="ist"
+                                                name={item.key}
+                                              />
+                                              <label className="form-check-label">{item.label}</label>
+                                            </div>
+                                    })}
+                                  </div>
                                 </div>
+                              </div>
+                              <div className="col-lg-6">
+                                <div className="form-group">
+                                  <label>MII</label>
+                                  <p>{this.props.errors.jenis_test}</p>
+                                  <div
+                                    className={`mt-0 form-control form-check form-check-inline d-inline-block ${this.props.errors.jenis_test ? "invalid" : ""}`}
+                                  >
+                                    {this.state.mii.map((item, index) => {
+                                      return <div key={index}>
+                                              <input type="checkbox" className="form-check-input"
+                                                id="mii"
+                                                name={item.key}
+                                              />
+                                              <label className="form-check-label">{item.label}</label>
+                                            </div>
+                                    })}
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                             <div className="modal-footer">
                               <Button small onClick={this.props.handleSubmit}>
